2236462550d188bdfd75594e0df5e9985f4f994a
Moritz Brödel <moritz.broedel@gmail.com>
2026-05-29 02:43:46 +0200

    Add Stylua to Zed

--- Common/apps/nvim/lua/config/options.lua
+++ Common/apps/nvim/lua/config/options.lua
@@ -7,5 +7,7 @@ vim.opt.expandtab = true
 -- vim.opt.tabstop = 4
 -- vim.opt.shiftwidth = 0 -- match tabstop
 
+vim.g.autoformat = false -- disable format on save
+
 vim.opt.spell = true
 vim.opt.spelllang = { "en", "de" }
